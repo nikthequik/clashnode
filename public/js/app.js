8a8c0aa434d1421b7d265baa0c11a7ac3af7ffd1
nikthequik <quik.the.nick@gmail.com>
2017-01-04 06:42:08 -0700

    add links to portfolio and facebook adjust various sizes

--- public/js/app.js
+++ public/js/app.js
@@ -14,8 +14,9 @@ angular.module('clashmash', ['ngRoute', 'ui.bootstrap'])
 .controller('HomeCtrl', ['$http', '$location', function($http, $location){
 	
   	var hc = this;
-  	hc.clanNotFound = false;
 	hc.getClans = function() {
+		hc.whichClanNotFound = "";
+		hc.clanNotFound = false;
 		var userID = hc.userClanID;
 		if (userID[0] !== '#') {
 			userID = '#' + userID;
@@ -23,7 +24,12 @@ angular.module('clashmash', ['ngRoute', 'ui.bootstrap'])
 		var url = 'clans/' + encodeURIComponent(userID);
 		$http.get(url)
 		.then(function(res) {
-			localStorage.setItem('userClan', JSON.stringify(res.data));
+			if (res.data.reason == "notFound") {
+				hc.whichClanNotFound = "User";
+				hc.clanNotFound = true;
+			} else {
+				hc.userClan = res.data;
+			}
 			var oppID = hc.oppClanID;
 			if (oppID[0] !== '#') {
 				oppID = '#' + oppID;
@@ -31,10 +37,17 @@ angular.module('clashmash', ['ngRoute', 'ui.bootstrap'])
 			var oppUrl = 'clans/' + encodeURIComponent(oppID);
 			$http.get(oppUrl)
 			.then(function(res){
-				console.log(res);
-				if (res.data.reason == "notFound") {
-					hc.clanNotFound = true;
-				} else {
+				if (res.data.reason == "notFound" || hc.clanNotFound) {
+					if (res.data.reason == "notFound" && hc.clanNotFound) {
+						hc.whichClanNotFound = "User and Opponent";
+					}
+					else if (res.data.reason == "notFound"){
+						hc.whichClanNotFound = "Opponent";
+						hc.clanNotFound = true;
+					}
+				}	
+				else {
+					localStorage.setItem('userClan', JSON.stringify(hc.userClan));
 					localStorage.setItem('oppClan', JSON.stringify(res.data));
 					localStorage.setItem('username', hc.username);
 					$location.path('/clashRoom');
@@ -52,7 +65,6 @@ angular.module('clashmash', ['ngRoute', 'ui.bootstrap'])
 	mc.username = localStorage.getItem('username');
 	var chat = angular.element(document.querySelector('#chat'));
 	var socket = io();
-
 	mc.userClan = JSON.parse(localStorage.getItem('userClan'));
 	mc.oppClan = JSON.parse(localStorage.getItem('oppClan'));
 	mc.members = mc.oppClan.memberList;
